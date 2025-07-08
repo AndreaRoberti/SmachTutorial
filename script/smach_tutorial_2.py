996118eefccebde4ae66e8913a98cc5c80c9cf6f
AndreaRoberti <andrea_kid90@yahoo.it>
2025-07-08 10:08:36 +0200

    [ADD] my name in tutorial_2

--- script/smach_tutorial_2.py
+++ script/smach_tutorial_2.py
@@ -7,8 +7,8 @@ class Foo(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
                              outcomes=['outcome1','outcome2'],
-                             input_keys=['foo_counter_in'],
-                             output_keys=['foo_counter_out'])
+                             input_keys=['foo_counter_in', 'foo_my_name'],
+                             output_keys=['foo_counter_out','foo_output_my_name'])
 
     def execute(self, userdata):
         print('Executing state FOO')
@@ -16,6 +16,8 @@ class Foo(smach.State):
             userdata.foo_counter_out = userdata.foo_counter_in + 1
             return 'outcome1'
         else:
+            print(userdata.foo_my_name)
+            userdata.foo_output_my_name = "FRANCESCO"
             return 'outcome2'
 
 
@@ -41,6 +43,7 @@ def main():
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['outcome4'])
     sm.userdata.sm_counter = 0
+    sm.userdata.my_name =  "ANDREA"
 
     # Open the container
     with sm:
@@ -48,8 +51,8 @@ def main():
         smach.StateMachine.add('FOO', Foo(), 
                                transitions={'outcome1':'BAR', 
                                             'outcome2':'outcome4'},
-                               remapping={'foo_counter_in':'sm_counter', 
-                                          'foo_counter_out':'sm_counter'})
+                               remapping={'foo_counter_in':'sm_counter','foo_my_name':'my_name',
+                                          'foo_counter_out':'sm_counter','foo_output_my_name':'my_name',})
         smach.StateMachine.add('BAR', Bar(), 
                                transitions={'outcome1':'FOO'},
                                remapping={'bar_counter_in':'sm_counter'})
@@ -57,7 +60,7 @@ def main():
 
     # Execute SMACH plan
     outcome = sm.execute()
-
+    print(sm.userdata.my_name)
 
 if __name__ == '__main__':
     main()
